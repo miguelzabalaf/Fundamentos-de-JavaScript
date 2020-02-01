a6c94f3b6b90b79f3f7090c12878656a03aa45c6
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-02-01 13:49:58 -0500

    He culminado la clase de variables de Platzi

---
+++ 01-Variables.js
@@ -0,0 +1,17 @@
+var nombre // DECLARO VARIABLE
+
+nombre = 'Miguel' // LE ASIGNO VALOR A LA VARIABLE DE TIPO STRING
+
+var saludo
+
+saludo = 'Hola'
+
+var concatenar = saludo + ' ' + nombre // CONCATENAMOS DOS VARIABLES
+
+console.log(concatenar)
+
+var num1 = 18
+var num2 = "18"
+
+num1 = 19 //  LE HE REASIGNADO EL VALOR A LA VARIABLE NUM1
+num2 = 18 // LE HE RASIGNADO EL VALOR DE TIPO STRING A TIPO INTEGER
