8c5cf94b1c9b0cddd344446ff81a5f5a64443de2
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-02-02 13:22:20 -0500

    He aprendido el uso de las variables numéricas, los modulos matemáticos posibles a usar, incremento y uso de decimales

---
+++ 03-VariablesNumeros.js
@@ -0,0 +1,17 @@
+var edad = 20
+
+edad = edad + 1  //FORMA 1 DE INCREMENTAR DE 1 EN UNO
+edad += 1 //FORMA 1 DE INCREMENTAR DE 1 EN UNO
+edad++ //FORMA 1 DE INCREMENTAR DE 1 EN UNO
+
+edad = edad - 1 // FORMA DE DESINCREMENTAR DE 1 EN UNO
+edad -= 1 // FORMA DE DESINCREMENTAR DE 1 EN UNO
+edad-- // FORMA DE DESINCREMENTAR DE 1 EN UNO
+
+
+var preciosDeVino = 200.3
+
+var total = preciosDeVino * 3
+
+console.log(Math.round(total)) // LLAMAMOS EL MODULO MATH PARA REDONDEAR EL RESULTADO
+console.log(total.toFixed(2)) // TENME EN CUENTA DOS DECIMALES DESPUÉS DE LA COMA
