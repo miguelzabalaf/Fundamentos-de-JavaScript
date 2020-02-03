805412ce902809d7b6cfeb276d936da1f30df5a0
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-02-03 09:34:43 -0500

    He aprendido el uso de los objetos e interactuando con ellos

---
+++ 06-Objetos.js
@@ -0,0 +1,22 @@
+var miguel = {
+    nombre: 'Miguel',
+    edad: 20,
+    altura: 1.70,
+    estudiando: true,
+    hobbies: ['Aprender', 'Hacer deporte', 'Escichar música']
+}
+
+
+function imprimirEnMayusculas (persona) {
+//  var { nombre } = persona
+    var nombre = persona.nombre
+    
+    console.log(nombre.toUpperCase())
+}
+
+// imprimirEnMayusculas(miguel)
+// UN OBEJTO ESTÁ CONSTITUIDO POR LLAVE Y VALOR
+
+function saludo (persona) {
+    console.log(`Hola ${persona.nombre}, tengo entendido que tienes ${persona.edad} años`)
+}
